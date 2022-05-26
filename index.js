9f64c39bc5cce3c17899d7549d4110c210ba2e5a
EyadDaGH <90962106+EyadDaGH@users.noreply.github.com>
2022-05-26 23:51:58 +0300

    ugub

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const inputBtn = document.getElementById("input-btn")
  let myLeads =[]
   const inputEl = document.getElementById("input")
   const ulEl = document.getElementById("ul-el")
+  const leadsFromLoacalStorage = JSON.parse(localStorage.getItem("myLeads"))
+  console.log(leadsFromLoacalStorage)
+  let x = Boolean(leadsFromLoacalStorage)
+  if(leadsFromLoacalStorage){
+  myLeads = leadsFromLoacalStorage
+  renderItems()}
   inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderItems()
+    console.log(localStorage.getItem("myLeads"))
 })
 function renderItems(){
     let listItems = ""
